c2cffb40db2c72fbd60f7bd8b61eb6ff9e6746b5
Artem Klinkov <art.klinkov@gmail.com>
2025-11-22 22:38:27 +0000

    KS-2-4 arithmetical operations

---
+++ src/main/kotlin/lesson_2/lesson2_task4.kt
@@ -0,0 +1,10 @@
+package org.example.lesson_2
+
+fun main() {
+    val diamonds = 7
+    val metal = 11
+    val buff = 20
+
+    println("%d".format(diamonds * buff / 100))
+    println("%d".format(metal * buff / 100))
+}
