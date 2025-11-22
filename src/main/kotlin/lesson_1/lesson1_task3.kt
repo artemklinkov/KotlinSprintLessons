88ed449773f6e3b2cd0c7867fd796eb01c594e0b
Artem Klinkov <art.klinkov@gmail.com>
2025-11-22 20:53:09 +0000

    KS-1-3 variables

---
+++ src/main/kotlin/lesson_1/lesson1_task3.kt
@@ -0,0 +1,16 @@
+package org.example.lesson_1
+
+fun main() {
+    val year: Int = 1961
+    var hour: Byte = 9
+    var minute: Byte = 7
+
+    println(year)
+    println(hour)
+    println(minute)
+
+    hour = 10
+    minute = 55
+
+    println("$hour:$minute")
+}
