8ffe8c5ba30d607582f4d036cf33d7ef0c72ddc4
Artem Klinkov <art.klinkov@gmail.com>
2025-11-22 20:29:13 +0000

    KS-1-2 variables

---
+++ src/main/kotlin/lesson_1/lesson1_task2.kt
@@ -0,0 +1,15 @@
+package org.example.lesson_1
+
+fun main() {
+    val ordersCount: Int = 10
+    val gratitudeMessage: String = "Thank you for your orders!"
+
+    println(ordersCount)
+    println(gratitudeMessage)
+
+//    val workersCount: Int = 2000
+//    println(workersCount)
+
+    val workersCount: Int = 1999
+    println(workersCount)
+}
