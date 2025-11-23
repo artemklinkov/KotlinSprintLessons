8493d5bbb6462c2f10910d7485b59bb7d6e21808
Artem Klinkov <art.klinkov@gmail.com>
2025-11-23 13:14:17 +0000

    KS-1-5 переименовать переменные, добавить константу

--- src/main/kotlin/lesson_1/lesson1_task5.kt
+++ src/main/kotlin/lesson_1/lesson1_task5.kt
@@ -1,9 +1,10 @@
 package org.example.lesson_1
 
 fun main() {
-    val secondsCount: Int = 1 * 60 * 60 + 30 * 60 + 9
-    val hours: Int = secondsCount / 3600
-    val minutes: Int = (secondsCount % 3600) / 60
-    val seconds: Int = secondsCount % 60
-    println("${"%02d".format(hours)}:${"%02d".format(minutes)}:${"%02d".format(seconds)}")
+    val secondsInOneMinute: Int = 60
+    val seconds: Int = 6480
+    val hours: Int = seconds / (secondsInOneMinute * secondsInOneMinute)
+    val minutes: Int = (seconds % (secondsInOneMinute * secondsInOneMinute)) / secondsInOneMinute
+    val secondsForPrint: Int = seconds % secondsInOneMinute
+    println("%02d:%02d:%02d".format(hours, minutes, secondsForPrint))
 }
