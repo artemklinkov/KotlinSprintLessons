d524ba5d90d46f0ab4d1b2f07f934beb05b396f0
Artem Klinkov <art.klinkov@gmail.com>
2025-11-23 11:50:12 +0000

    KS-3-1 strings

---
+++ src/main/kotlin/lesson_3/lesson3_task1.kt
@@ -0,0 +1,10 @@
+package org.example.lesson_3
+
+fun main() {
+    val userName = "John"
+    val dayGreeting = "день"
+    val nightGreeting = "вечер"
+
+    println("Добрый $dayGreeting, $userName")
+    println("Добрый $nightGreeting, $userName")
+}
