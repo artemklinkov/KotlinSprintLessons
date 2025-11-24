823aeca493d0b49747dac12e91ac75b2dbb1c0fb
Artem Klinkov <art.klinkov@gmail.com>
2025-11-24 12:48:18 +0000

    KS-2-1 вынести оценки в переменные, изменить тип переменной с количеством учеников

--- src/main/kotlin/lesson_2/lesson2_task1.kt
+++ src/main/kotlin/lesson_2/lesson2_task1.kt
@@ -1,6 +1,11 @@
 package org.example.lesson_2
 
 fun main() {
-    val avg: Float = (3 + 4 + 3 + 5) / 4.0f
+    val mark1 = 3.0f
+    val mark2 = 4.0f
+    val mark3 = 3.0f
+    val mark4 = 5.0f
+    val count = 4.0f
+    val avg: Float = (mark1 + mark2 + mark3 + mark4) / count
     println("%.2f".format(avg))
 }
