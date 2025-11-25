c7f29a3e37a6ab5f66a7a24d8cd716b8abe97cfe
Artem Klinkov <art.klinkov@gmail.com>
2025-11-25 18:50:45 +0000

    KS-2-1 изменен тип переменной

--- src/main/kotlin/lesson_2/lesson2_task1.kt
+++ src/main/kotlin/lesson_2/lesson2_task1.kt
@@ -5,7 +5,7 @@ fun main() {
     val mark2 = 4.0f
     val mark3 = 3.0f
     val mark4 = 5.0f
-    val count = 4.0f
+    val count = 4
     val avg: Float = (mark1 + mark2 + mark3 + mark4) / count
     println("%.2f".format(avg))
 }
