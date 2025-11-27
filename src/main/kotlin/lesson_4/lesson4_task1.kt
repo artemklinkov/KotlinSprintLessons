0bfb2e81884ee4288e18b69fa48587b151444b6d
Artem Klinkov <art.klinkov@gmail.com>
2025-11-27 15:04:09 +0000

    KS-4-1 вынести количество столов в константу

--- src/main/kotlin/lesson_4/lesson4_task1.kt
+++ src/main/kotlin/lesson_4/lesson4_task1.kt
@@ -1,9 +1,11 @@
 package org.example.lesson_4
 
+const val NUMBER_OF_TABLES = 13
+
 fun main() {
-    val tables = 13
     val tablesReservedToday = 13
     val tablesReservedTomorrow = 9
 
-    println("Доступность столиков на сегодня: ${tables > tablesReservedToday},\nДоступность столиков на завтра: ${tables > tablesReservedTomorrow}.")
+    println("Доступность столиков на сегодня: ${tablesReservedToday < NUMBER_OF_TABLES}," +
+            "\nДоступность столиков на завтра: ${tablesReservedTomorrow < NUMBER_OF_TABLES}.")
 }
