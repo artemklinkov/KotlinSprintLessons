3b1e3521e16c75607915d5857a02d54d37c9c210
Artem Klinkov <art.klinkov@gmail.com>
2025-11-27 10:47:17 +0000

    KS-3-5 изменить на использование split

--- src/main/kotlin/lesson_3/lesson3_task5.kt
+++ src/main/kotlin/lesson_3/lesson3_task5.kt
@@ -2,9 +2,11 @@ package org.example.lesson_3
 
 fun main() {
     val move = "D2-D4;0"
-    val from = move.substringBefore("-")
-    val to = move.substringAfter("-").substringBefore(";")
-    val count = move.substringAfter(";").toInt()
+    val list = move.split("-", ";")
+
+    val from = list[0]
+    val to = list[1]
+    val count = list[2].toInt()
     println(from)
     println(to)
     println(count)
