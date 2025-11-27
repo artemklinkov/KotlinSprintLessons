894fe3e50b16193b4817ec39e978c3ea5eb9b8b1
Artem Klinkov <art.klinkov@gmail.com>
2025-11-27 15:11:57 +0000

    KS-4-2 вынести параметры в константы

--- src/main/kotlin/lesson_4/lesson4_task2.kt
+++ src/main/kotlin/lesson_4/lesson4_task2.kt
@@ -1,12 +1,21 @@
 package org.example.lesson_4
 
+const val WEIGHT_LOWER = 35
+const val WEIGHT_UPPER = 100
+const val VOLUME_UPPER = 100
+
 fun main() {
-    val weight = 42
-    val volume = 120
+    var weight = 20
+    var volume = 80
 
-    val weightLower = 35
-    val weightUpper = 100
-    val volumeUpper = 100
 
-    println("Груз с весом $weight кг и объемом $volume л соответствует категории 'Average': ${weight > weightLower && weight <= weightUpper && volume < volumeUpper}")
+
+    println("Груз с весом $weight кг и объемом $volume л " +
+            "соответствует категории 'Average': ${weight > WEIGHT_LOWER && weight <= WEIGHT_UPPER && volume < VOLUME_UPPER}")
+
+    weight = 50
+    volume = 100
+    println("Груз с весом $weight кг и объемом $volume л " +
+            "соответствует категории 'Average': ${weight > WEIGHT_LOWER && weight <= WEIGHT_UPPER && volume < VOLUME_UPPER}")
+
 }
