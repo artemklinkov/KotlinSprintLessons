7a4f392e8accda86905011ee7bc7ea688ea15b18
Artem Klinkov <art.klinkov@gmail.com>
2025-11-27 09:15:33 +0000

    KS-3-4 добавлен номер хода

--- src/main/kotlin/lesson_3/lesson3_task4.kt
+++ src/main/kotlin/lesson_3/lesson3_task4.kt
@@ -3,11 +3,12 @@ package org.example.lesson_3
 fun main() {
     var from = "E2"
     var to = "E4"
-
-    println("$from:$to")
+    var move = 1
+    println("$from-$to;$move")
 
     from = "D2"
     to = "D3"
+    move = 2
 
-    println("$from:$to")
+    println("$from-$to;$move")
 }
