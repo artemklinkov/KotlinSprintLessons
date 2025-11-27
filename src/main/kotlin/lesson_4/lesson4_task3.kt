267994f189b1fd023dbc89ff8c6f46660987377f
Artem Klinkov <art.klinkov@gmail.com>
2025-11-27 20:15:02 +0000

    KS-4-3 вынести значения в константы

--- src/main/kotlin/lesson_4/lesson4_task3.kt
+++ src/main/kotlin/lesson_4/lesson4_task3.kt
@@ -1,10 +1,12 @@
 package org.example.lesson_4
-
+    const val HUMIDITY_THRESHOLD = 20
+    const val DEFAULT_SEASON = "Winter"
 fun main() {
     val isSunny = true
     val isOpened = true
     val humidity = 20
     val season = "Winter"
 
-    println("Благоприятные ли условия сейчас для роста бобовых? ${isSunny && isOpened && humidity == 20 && season != "Winter"}")
+    println("Благоприятные ли условия сейчас для роста бобовых? " +
+            "${isSunny && isOpened && humidity == HUMIDITY_THRESHOLD && season != DEFAULT_SEASON}")
 }
