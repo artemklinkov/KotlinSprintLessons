4330b69fcecf8fa30a69e55ddc8cb1a6358ea90e
Artem Klinkov <art.klinkov@gmail.com>
2025-11-27 20:12:45 +0000

    KS-4-2 перенести строки

--- src/main/kotlin/lesson_4/lesson4_task2.kt
+++ src/main/kotlin/lesson_4/lesson4_task2.kt
@@ -8,14 +8,12 @@ fun main() {
     var weight = 20
     var volume = 80
 
-
-
-    println("Груз с весом $weight кг и объемом $volume л " +
-            "соответствует категории 'Average': ${weight > WEIGHT_LOWER && weight <= WEIGHT_UPPER && volume < VOLUME_UPPER}")
+    println("Груз с весом $weight кг и объемом $volume л соответствует категории 'Average': " +
+            "${weight > WEIGHT_LOWER && weight <= WEIGHT_UPPER && volume < VOLUME_UPPER}")
 
     weight = 50
     volume = 100
-    println("Груз с весом $weight кг и объемом $volume л " +
-            "соответствует категории 'Average': ${weight > WEIGHT_LOWER && weight <= WEIGHT_UPPER && volume < VOLUME_UPPER}")
+    println("Груз с весом $weight кг и объемом $volume л соответствует категории 'Average': " +
+            "${weight > WEIGHT_LOWER && weight <= WEIGHT_UPPER && volume < VOLUME_UPPER}")
 
 }
