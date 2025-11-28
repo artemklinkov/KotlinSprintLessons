a1c32de100b46411b075e5266eb37b5fab6a1da0
Artem Klinkov <art.klinkov@gmail.com>
2025-11-28 19:43:38 +0000

    KS-5-3 инициализировать начальные данные рандомными значениями

--- src/main/kotlin/lesson5/lesson5_task3.kt
+++ src/main/kotlin/lesson5/lesson5_task3.kt
@@ -3,14 +3,16 @@ package org.example.lesson5
 import kotlin.random.Random
 
 fun main() {
-    val numbers = intArrayOf(Random.nextInt(0, 42),
-        Random.nextInt(0, 42))
+    val numbers = intArrayOf(
+        (0..42).random(),
+        (0..42).random(),
+    )
     println("Введите числа от 0 до 42:")
     val userFirstNumber = readln().toInt()
     val userSecondNumber = readln().toInt()
 
     val result = when {
-        userFirstNumber in numbers && userSecondNumber in numbers  -> "Поздравляем! Вы выиграли главный приз!"
+        userFirstNumber in numbers && userSecondNumber in numbers -> "Поздравляем! Вы выиграли главный приз!"
         userFirstNumber in numbers || userSecondNumber in numbers -> "Вы выиграли утешительный приз!"
         else -> "Неудача!"
     }
