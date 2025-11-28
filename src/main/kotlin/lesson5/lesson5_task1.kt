7ae7ded969d845670c3684e82a3736a4d302c3d9
Artem Klinkov <art.klinkov@gmail.com>
2025-11-28 19:46:19 +0000

    KS-5-1 использовать другой рандом, удалить импорт

--- src/main/kotlin/lesson5/lesson5_task1.kt
+++ src/main/kotlin/lesson5/lesson5_task1.kt
@@ -1,14 +1,13 @@
 package org.example.lesson5
 
-import kotlin.random.Random
-
 fun main() {
-    val a = Random.nextInt(0, 10)
-    val b = Random.nextInt(0, 10)
+    val a = (0..10).random()
+    val b = (0..10).random()
 
     println("Для продолжения решите математическую задачу: $a + $b = ?")
     val userAnswer = readln().toInt()
     val correctAnswer = a + b
+
     if (userAnswer == correctAnswer) {
         println("Добро пожаловать!")
     } else {
